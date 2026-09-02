09b535fc41e272bb0fabd9957635f3da9d943fdf
Kavya Sri <srikavya1609@gmail.com>
2026-09-02 21:09:37 +0530

    Fix frontend API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://online-code-judge-pdik.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
